db9d379c06d7ad4642f213fcfd28cab1f801d6ff
disconcent <89609518+disconcent@users.noreply.github.com>
2023-12-06 17:45:12 +0700

    Update config.py

--- ot1/config.py
+++ ot1/config.py
@@ -39,7 +39,7 @@ UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 DB_URI = os.environ.get("DATABASE_URL", "postgres://xnawaiti:Qo3GdMGamgNqLPFZXABJg-YyjfOMSXIn@suleiman.db.elephantsql.com/xnawaiti")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001538791859"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002015487480"))
 FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1001801421469"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
